03564ffc35eaefcd96c8ff67b01f3282fd3d252f
xm8-CODEX <it25.vishal.rajak@kccemsr.edu.in>
2026-08-18 01:49:41 +0530

    Delete backend directory

--- backend/src/main/java/com/jobportal/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.jobportal.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
